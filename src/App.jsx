3dafac2937ff73e79b7feccc4042b1532a3eef84
masakifujie <yan.yan.yan@icloud.com>
2021-04-19 09:23:18 +0900

    質問ボタン作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import defaultDataset from "./dataset";
-// import { AssessmentTwoTone } from "@material-ui/icons";
-// import { style } from "@material-ui/system";
+import { AssessmentTwoTone } from "@material-ui/icons";
+import { style } from "@material-ui/system";
 import "./assets/styles/style.css";
+import { AnswersList } from "./components/index";
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -14,12 +15,28 @@ export default class App extends React.Component {
       open: false,
     };
   }
+
+  initAnswer = () => {
+    const initDataset
+      = this.state.dataset[this.state.currentId];
+    const initAnswers
+      = initDataset.answers;
+
+    this.setState({
+      answers: initAnswers,
+    });
+  };
+
+  componentDidMount() {
+    this.initAnswer();
+  }
   render() {
     return (
-      <div>
-        <section className="c-section"></section>
-        <div className="c-box"></div>
-      </div>
+      <section className="c-section">
+        <div className="c-box">
+          <AnswersList answers={this.state.answers} />
+        </div>
+      </section>
     );
   }
 }
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as AnswersList } from './AnswersList'
+export { default as Answers } from './Answer'
+export {default as Answer} from './Answer'
